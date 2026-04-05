a6b2dc707b00c2019776a6e1dcb160564a3975e8
Alvis Lu <uualvislu@gmail.com>
2026-04-05 20:40:21 +0800

    feat: add image remote patterns for Supabase in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["172.20.10.12"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
